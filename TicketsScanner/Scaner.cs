5eee9b4779affea7eaf4ce2126d8a405536396ad
Timur <2001rich04@gmail.com>
2023-01-27 20:09:05 +0300

    downgrate to .net 5

--- TicketsScanner/Scaner.cs
+++ TicketsScanner/Scaner.cs
@@ -1,64 +1,66 @@
-﻿namespace TicketsScanner;
-using System.IO.Ports;
-
-public class Scanner
+﻿namespace TicketsScanner
 {
-    public string LastTicket { get; private set; }
+    using System.IO.Ports;
 
-    private readonly string _comPort;
-    private SerialPort _serialPort;
-
-    public Scanner(string comPort)
+    public class Scanner
     {
-        _comPort = comPort;
-    }
+        public string LastTicket { get; private set; }
 
-    public bool Start()
-    {
-        try
+        private readonly string _comPort;
+        private SerialPort _serialPort;
+
+        public Scanner(string comPort)
         {
+            _comPort = comPort;
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                LastTicket = string.Empty;
+
+                _serialPort = new SerialPort(_comPort);
+
+                _serialPort.BaudRate = 9600;
+                _serialPort.Parity = Parity.None;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.DataBits = 8;
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.RtsEnable = true;
+                _serialPort.Open();
+
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                return true;
+            }
+            catch (Exception message)
+            {
+                return false;
+            }
+        }
+
+        public void Stop()
+        {
+            _serialPort.Close();
+        }
+
+        public string GetNewTicket()
+        {
+            var temp = LastTicket;
             LastTicket = string.Empty;
+            Stop();
 
-            _serialPort = new SerialPort(_comPort);
-
-            _serialPort.BaudRate = 9600;
-            _serialPort.Parity = Parity.None;
-            _serialPort.StopBits = StopBits.One;
-            _serialPort.DataBits = 8;
-            _serialPort.Handshake = Handshake.None;
-            _serialPort.RtsEnable = true;
-            _serialPort.Open();
-
-            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-
-            return true;
+            return temp;
         }
-        catch (Exception message)
+
+        private void DataReceivedHandler(
+                                object sender,
+                                SerialDataReceivedEventArgs e)
         {
-            return false;
+            SerialPort sp = (SerialPort)sender;
+
+            LastTicket = sp.ReadExisting();
         }
-    }
-
-    public void Stop()
-    {
-        _serialPort.Close();
-    }
-
-    public string GetNewTicket()
-    {
-        var temp = LastTicket;
-        LastTicket = string.Empty;
-        Stop();
-
-        return temp;
-    }
-
-    private void DataReceivedHandler(
-                            object sender,
-                            SerialDataReceivedEventArgs e)
-    {
-        SerialPort sp = (SerialPort)sender;
-
-        LastTicket = sp.ReadExisting();
     }
 }
